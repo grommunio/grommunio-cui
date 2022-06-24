09d3b7be01b63496b82600651038b75b1a492f0c
Noah Schmidt <noah.schmidt@grommunio.com>
2022-06-24 19:04:37 +0200

    Add black to ap8

--- ap8.py
+++ ap8.py
@@ -3,6 +3,8 @@ import os
 from pathlib import Path
 
 import autopep8 as ap8
+import black
+from black import Mode, NothingChanged, InvalidInput
 
 
 def fix_file(filename):
@@ -38,7 +40,7 @@ def fix_file(filename):
 
 def main():
     cur_path = '.'
-    process_walk(cur_path)
+    process_walk(cur_path, False)
     print('Ende')
 
 
@@ -49,10 +51,23 @@ def process_walk(cur_path, inplace=True):
             if not f[-2:] == 'py' or f[-6:-3] == 'opt':
                 continue
             fix = fix_file(f"{t[0]}{os.sep}{f}")
+            mode = Mode(
+                line_length=79
+            )
+            try:
+                fix2 = black.format_file_contents(fix, fast=False, mode=mode)
+            except NothingChanged as e:
+                print(f"Error while processing {f}: {e}")
+                fix2 = fix
+            except InvalidInput as e:
+                print(f"Error while processing {f}: {e}")
+                fix2 = fix
             opt = '_opt' if not inplace else ''
             new_f = f"{t[0]}{os.sep}{os.path.splitext(f)[0]}{opt}{os.path.splitext(f)[1]}"
             with open(new_f, 'w') as w:
-                w.write(fix)
+                nl = '\n'
+                print(f"before fix2 was {fix.count(nl)} and now is {fix2.count(nl)}")
+                w.write(fix2)
                 w.close()
                 print(f"Optimized file {new_f} written.")
 
