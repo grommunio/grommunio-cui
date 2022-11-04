d01f02fbdfe825180ea1b33e40c650783fc53251
Noah Schmidt <noah.schmidt@grommunio.com>
2022-11-04 19:11:08 +0100

    Add vim search and replace output ready to use for copy n paste.

--- localize_it.sh
+++ localize_it.sh
@@ -13,6 +13,11 @@ done
 
 echo "The po files has been generated in $(cd locale && echo */) languages."
 echo "Please translate them and run 'make'"
+echo "You can use '/^msgstr \"\"\\n\\n' in vi to find all new empty strings"
+echo 'and you can use `:%s#^msgid "\(.*\)"\nmsgstr ""\n\n#msgid ""\r"\1"\rmsgstr ""\r"\1"\r\r#` to translate the english file automatically by keys.'
+echo 'Use `:%s#^msgid ""\n"\(.*\)"\nmsgstr ""\n\n#msgid ""\r"\1"\rmsgstr ""\r"\1"\r\r#` to translate keys longer than two lines.'
+echo 'Use `:%s#^msgid ""\n"\(.*"\n".*\)"\nmsgstr ""\n\n#msgid ""\r"\1"\rmsgstr ""\r"\1"\r\r#` to translate keys longer than three lines.'
+echo 'Use `:%s#^msgid ""\n"\(.*"\n".*"\n".*\)"\nmsgstr ""\n\n#msgid ""\r"\1"\rmsgstr ""\r"\1"\r\r#` to translate keys longer than four lines.'
 
 # old stuff, better use make
 exit 0
