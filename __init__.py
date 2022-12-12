3b17d33975d85e28b801c1e436d1c9d3856ece7f
Noah Schmidt <noah.schmidt@grommunio.com>
2022-12-12 13:42:48 +0000

    code: Get 10 points on pylint.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """The console user interface (cui) is a console utility for configuring grommunio"""
+# Disable pylint warning about module name not being in snake_case naming style.
+# pylint: disable=invalid-name
 import cui
 
 if __name__ == "__main__":
